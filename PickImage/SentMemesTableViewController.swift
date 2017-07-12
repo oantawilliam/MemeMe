5edf7683093e43f1bb2411964aecd30c4551fd48
William Oanta <william.oanta@Williams-MacBook-Pro.local>
2017-07-12 20:21:20 +0300

    Implemented MemeMe 2.0

---
+++ PickImage/SentMemesTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SentMemesViewController.swift
+//  MemeMe
+//
+//  Created by William Oanta on 12/07/2017.
+//  Copyright © 2017 William Oanta. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
